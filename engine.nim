20f20104ba1651d35a6df39aff162728be061c30
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-21 23:55:49 +0200

    chg: Move to better structure sketch

---
+++ engine.nim
@@ -0,0 +1,6 @@
+#:__________________________________________________________________
+#  oQ3  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv2 or later  |
+#:__________________________________________________________________
+# @fileoverview Cable connector to all of the Engine's buildsystem modules
+import ./engine/src ; export src
+import ./engine/dir ; export dir
